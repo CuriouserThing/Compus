acc419e72a82e1a1eed7b2a9354f87d4ca35e08b
TheMostCuriousThing <adelaidekwalter@gmail.com>
2022-04-21 17:52:59 -0400

    Add implicit Emoji operators.

--- src/Compus/Models/Emoji.cs
+++ src/Compus/Models/Emoji.cs
@@ -1,33 +1,42 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace Compus.Models
+namespace Compus.Models;
+
+/// <summary>
+///     https://discord.com/developers/docs/resources/emoji#emoji-object
+/// </summary>
+public record Emoji
 {
-    /// <summary>
-    ///     https://discord.com/developers/docs/resources/emoji#emoji-object
-    /// </summary>
-    public record Emoji
+    public Snowflake? Id { get; init; }
+
+    public string? Name { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public Option<IReadOnlyList<Role>> Roles { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public Option<User> User { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public Option<bool> RequireColons { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public Option<bool> Managed { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public Option<bool> Animated { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public Option<bool> Available { get; init; }
+
+    public static implicit operator Emoji(Snowflake id)
     {
-        public Snowflake? Id { get; init; }
+        return new Emoji { Id = id };
+    }
 
-        public string? Name { get; init; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public Option<IReadOnlyList<Role>> Roles { get; init; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public Option<User> User { get; init; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public Option<bool> RequireColons { get; init; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public Option<bool> Managed { get; init; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public Option<bool> Animated { get; init; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        public Option<bool> Available { get; init; }
+    public static implicit operator Emoji(string name)
+    {
+        return new Emoji { Name = name };
     }
 }
