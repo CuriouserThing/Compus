a77361b0cf3d116c8cba20e63b97c26ebfbaa87c
TheMostCuriousThing <adelaidekwalter@gmail.com>
2022-04-21 18:21:18 -0400

    Make Identity DI-friendly.

--- src/Compus/Gateway/Identity.cs
+++ src/Compus/Gateway/Identity.cs
@@ -2,16 +2,23 @@ namespace Compus.Gateway
 {
     public class Identity
     {
-        public Identity(string token, GatewayIntents intents = GatewayIntents.None)
+        public Identity(Token token, GatewayIntents intents)
         {
             Token   = token;
             Intents = intents;
         }
 
-        public string Token { get; }
+        public Identity(Token token, GatewayIntents intents, Option<Shard> shard)
+        {
+            Token   = token;
+            Intents = intents;
+            Shard = shard;
+        }
+
+        public Token Token { get; }
 
         public GatewayIntents Intents { get; }
 
-        public Option<Shard> Shard { get; init; }
+        public Option<Shard> Shard { get; }
     }
 }
